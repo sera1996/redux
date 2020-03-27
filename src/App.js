88921bcb0886d400f514193daea60f2d31e39c11
sera1996 <narumoto65@hotmail.com>
2020-03-27 20:36:39 +0900

    savew[

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
+import {connect} from 'react-redux';
 import './App.css';
 
-function App() {
+import {increment,decrement} from './actions'
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,5 +24,23 @@ function App() {
     </div>
   );
 }
+*/
 
-export default App;
+class App extends React.Component {
+  render(){
+    const props = this.props
+    return(
+      <React.Fragment>
+        <div>value:{props.value}</div>
+        <button onClick = {props.increment}>+1</button>
+        <button onClick = {props.decrement}>-1</button>
+      </React.Fragment>
+    )
+  }
+}
+const mapStateToProps = state => ({value: state.count.value})
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement())
+})
+export default connect(mapStateToProps,mapDispatchToProps)(App)
